ff64758192b7c6208c360b89df5fee8ed508809b
jjblues86 <jeromejoof@yahoo.com>
2019-05-07 22:13:14 -0700

    creating notes

--- routing/src/Dashboard/Dashboard.js
+++ routing/src/Dashboard/Dashboard.js
@@ -1,29 +1,53 @@
 import React from 'react';
+import NoteForm from '../NoteCreateForm/NoteForm';
 
 export default class Dashboard extends  React.Component {
     constructor(props) {
         super(props);
 
         this.state = {};
-        // Jerome - this array in here will all the NoteCreateForm for my application
+        // Jerome - this array in here will hold all the NoteCreateForm for my application
         this.state.notes = [];
     }
 
-    renderNotes = () => {
+    addNotes = () => {
         return(
             <ul>
-                Welcome :)
+                {
+                    this.state.notes.map(currentNotes => {
+                        return <li key={currentNotes.id}>
+                            {currentNotes.title} : {currentNotes.content}
+                        </li>
+                    })
+                }
             </ul>
         )
 
+    };
+
+    handleAddNote = note => {
+        //Jerome - this is where I create the id, content and title
+        // note.id = Math.random();
+        // note.createdOn = new Date();
+
+        this.setState((previousState) => {
+            return {
+                notes: [...previousState.notes, {
+                    ...note,
+                id: Math.random(),
+                createdOn: new Date(),
+                }],
+            }
+        });
     };
 
 
     render() {
         return(
             <main>
-              <h2>Create Notes</h2>
-                {this.renderNotes()}
+                <h2>Create Notes</h2>
+                <NoteForm handleAddNote={this.handleAddNote}/>
+                {this.addNotes()}
             </main>
         )
     }
