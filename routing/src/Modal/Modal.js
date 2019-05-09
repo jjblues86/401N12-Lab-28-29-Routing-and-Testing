8f00d2a8f4fafd94b2ba1ea412eed676c227336e
jjblues86 <jeromejoof@yahoo.com>
2019-05-08 18:20:15 -0700

    working on modal

---
+++ routing/src/Modal/Modal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Modal extends React.Component {
+    render(){
+        return(
+            <div className={showHide}>
+                <div className="modal-main">
+                    <button>Close</button>
+        </div>
+            </div>)
+    }
+}
